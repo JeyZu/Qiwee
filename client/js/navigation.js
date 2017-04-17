50af75a9ce96a73ac85ce449ed1d75b89a7420b4
JeyZu <jeremie.quatorze@gmail.com>
2017-04-17 15:52:32 +0200

    f

--- client/js/navigation.js
+++ client/js/navigation.js
@@ -1,15 +1,12 @@
 Template.navigation.helpers({
-	activeRouteClass: function() {
-		var args = Array.prototype.slice.call(argument, 0);
-		args.pop();
-
-		var active = _.any(args, function(name) {
-			return Router.current() && Router.current().route.getName() === name
-		});
-
-		console.log(active);
-		console.log(args);
-
-		return active && 'active';
-	}
+	activeRouteClass: function(/* route names */) {
+		var args = Array.prototype.slice.call(arguments, 0);
+    	args.pop();
+    
+    	var active = _.any(args, function(name) {
+      		return Router.current() && Router.current().route.getName() === name
+    	});
+    
+    	return active && 'active';
+  	}
 });
