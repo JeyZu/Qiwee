7499b0286ccdf753d7e561366f8ba4eb8a80e895
JeyZu <jeremie.quatorze@gmail.com>
2017-04-19 17:22:40 +0200

    AnimationPage

--- client/template/application/layout.js
+++ client/template/application/layout.js
@@ -3,3 +3,20 @@ Template.main.events({
 		Meteor.logout();
 	}
 });
+
+Template.layout.onRendered(function() {
+	this.find('#main')._uihooks = {
+		insertElement: function(node, next) {
+			$(node)
+				.hide()
+				.insertBefore(next)
+				.fadeIn();
+		},
+		removeElement: function(node) {
+			$(node).fadeOut(function() {
+				$(this).remove();
+			});
+		}
+	}
+
+});
