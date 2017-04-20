ebc7f33a2fd44662dbc04026e9754045c79efdf6
JeyZu <jeremie.quatorze@gmail.com>
2017-04-20 10:53:25 +0200

    s

--- client/template/application/layout.js
+++ client/template/application/layout.js
@@ -3,20 +3,3 @@ Template.main.events({
 		Meteor.logout();
 	}
 });
-
-
-Template.layout.onRendered(function() {
-	this.find('#main')._uihooks = {
-   		insertElement: function(node, next) {
-   		  $(node)
-   		    .hide()
-    	    .insertBefore(next)
-    	    .fadeIn();
-    	},
-    	removeElement: function(node) {
-    	  $(node).fadeOut(function() {
-    	    $(this).remove();
-    	  });
-    	}
-  	}
-});
