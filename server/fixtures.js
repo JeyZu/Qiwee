341aa0d010f9ba6017e0ea26f4314ddc8d54919c
JeyZu <jeremie.quatorze@gmail.com>
2017-04-20 15:16:28 +0200

    fixtures

---
+++ server/fixtures.js
@@ -0,0 +1,34 @@
+
+// Fixture data 
+if (Posts.find().count() === 0) {
+  var now = new Date().getTime();
+
+  // create two users
+
+  var sachaId = Accounts.createUser({
+    profile: {
+      name: 'Jérémie Quatorze'
+    },
+    username: "JeyZu",
+    email: "jeyzu@example.com",
+  });
+
+  var sacha = Meteor.users.findOne(sachaId);
+
+  var telescopeId = Posts.insert({
+    title: 'Introducing Qiwee',
+    userId: sacha._id,
+    author: sacha.profile.name,
+    message: 'Qiwee!',
+    submitted: new Date(now - 7 * 3600 * 1000),
+    commentsCount: 1,
+    upvoters: [], votes: 0
+  });
+
+  Comments.insert({
+    postId: telescopeId,
+    userId: sacha._id,
+    author: sacha.profile.name,
+    submitted: new Date(now - 5 * 3600 * 1000),
+    body: 'Interesting project, can I get involved?'
+  });
