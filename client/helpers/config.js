fa229240f792183fea8bb9167d2ab406e240ce88
JeyZu <jeremie.quatorze@gmail.com>
2017-04-30 17:38:30 +0200

    test

--- client/helpers/config.js
+++ client/helpers/config.js
@@ -1,4 +1,14 @@
 Accounts.ui.config({
-	passwordSignupFields: 'USERNAME_ONLY'
+	passwordSignupFields: 'USERNAME_ONLY',
+	requestPermissions: {
+	    twitch: [
+	    	'user_read',
+	    	'channel_read',
+	    	'channel_editor',
+	    	'channel_commercial',
+	    	'channel_subscriptions',
+	    	'channel_check_subscription'
+	    ]
+	}
 });
 accountsUIBootstrap3.setLanguage('fr');
---
+++ client/template/twitch/home.js
@@ -0,0 +1,5 @@
+new Twitch.Player('twitch_channel', {
+	width: 1920,
+	height: 1080,
+	channel: "qiweetv",
+});
