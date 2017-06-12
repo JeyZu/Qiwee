3f9c571eccedff9111a30b86fbb39b36ec23ed42
JeyZu <jeremie.quatorze@gmail.com>
2017-06-13 00:54:44 +0200

    zda

--- client/helpers/config.js
+++ client/helpers/config.js
@@ -1,14 +1,4 @@
 Accounts.ui.config({
 	passwordSignupFields: 'USERNAME_ONLY',
-	requestPermissions: {
-	    twitch: [
-	    	'user_read',
-	    	'channel_read',
-	    	'channel_editor',
-	    	'channel_commercial',
-	    	'channel_subscriptions',
-	    	'channel_check_subscription'
-	    ]
-	}
 });
 accountsUIBootstrap3.setLanguage('fr');
--- client/template/twitch/home.js
+++
@@ -1,5 +0,0 @@
-new Twitch.Player('twitch_channel', {
-	width: 1920,
-	height: 1080,
-	channel: "qiweetv",
-});
